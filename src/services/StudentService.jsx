823576345f04749dc22abce740d239a1f23a11f4
Daniel <mewregi@gmail.com>
2021-12-29 17:17:35 -0800

    added heroku capability

--- src/services/StudentService.jsx
+++ src/services/StudentService.jsx
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const STUDENT_API_BASE_URL = "http://localhost:8080/api/v1/students";
+const STUDENT_API_BASE_URL = "https://studentsystem-backend.herokuapp.com//api/v1/students";
 
 class StudentService {
 
